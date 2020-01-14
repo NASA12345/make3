42356eb154ffb83dfeff72fb8869a29746a8ddb2
NASA12345 <nayan11404@gmail.com>
2020-01-14 13:14:06 +0530

    [GCI-<Nayan>] message
    [GCI-<Nayan>] message
    
    [GCI-<Nayan>] message
    
    [GCI-<Nayan>] message

---
+++ api/routes/categories.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const dataService = require('./../../dataService.js');
+
+// Getting all the products under a category based on category's ID
+router.get('/:ctyId', (req, res, next) => {
+  const { ctyId } = req.params
+  const productsforparticularCategory = Object.fromEntries(
+    Array.from(dataService.getCombinedProductMap()).filter(([productId, product]) => product.categoryId === ctyId) || [])
+  res.status(200).json(productsforparticularCategory)
+  // If id entered is Invalid ID otherwise result.
+  if (!productsforparticularCategory) {
+    return res.send('Invalid category ID');
+  }
+  res.json(productsforparticularCategory);
+});
+
+module.exports = router;
+// The whole project has a server 3000 port named in server.js file.
---
+++ api/routes/products.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const dataService = require('./../../dataService.js');
+
+// It will combine all products
+router.get('/all', (req, res, next) => {
+  res.status(200)
+  res.json(Object.fromEntries(dataService.getCombinedProductMap())
+  )
+})
+
+// It will show specific product
+router.get('/:id', (req, res) => {
+  const productData = dataService.getProducts();
+  const productById = productData.get(req.params.id);
+  // If id enter is Invalid otherwise result.
+  if (!productById) {
+    return res.send('Invalid product ID');
+  }
+  res.json(productById);
+});
+
+module.exports = router;
+// The whole project has a server 3000 port named in server.js file.
